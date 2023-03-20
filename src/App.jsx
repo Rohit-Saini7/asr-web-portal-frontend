e02375046fb38a78f1a4507a7d6f8b1a26a5fa70
Rohit <95734284+Rohit-Saini7@users.noreply.github.com>
2023-03-20 11:56:41 +0530

    Design changes (#16)
    * Test
    
    * Design Changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,8 @@ import Login from './pages/Login';
 import Preview from './pages/Preview';
 
 /*
- ✅ TODO: 1. Remove source and destination lang from transcript.
- ✅ TODO: 2. Add another tab for Video to Video conversion.
- ✅ TODO: 3. Preview for TTS.
- ✅ TODO: 4. Make dictionaries to work again.
-  TODO: 5. Add colaboration of docs.
-  TODO: 6. Some design Changes required.
-  TODO: 7. Deployment on AWS.
-
+  TODO: 1. dropdown for using a previously uploaded/generated file
+  TODO: 2. Deployment on AWS.
 */
 
 export default function App() {
--- src/components/HomeTabs.jsx
+++ src/components/HomeTabs.jsx
@@ -1,33 +1,31 @@
-import React from 'react';
 import styled from 'styled-components';
 
 const HomeTabs = ({ setTabSelected }) => {
   const handleClick = (e) => {
-    const tabs = document.querySelectorAll('.tabs');
-    tabs.forEach((item) => {
+    document.querySelectorAll('.tabs').forEach((item) => {
       item.classList.remove('active');
     });
-    setTabSelected(
-      (lastValue) =>
-        lastValue !== e.target.attributes.name.value &&
-        e.target.attributes.name.value
-    );
+    const eleName = e.target.attributes.name.value;
+    setTabSelected((p) => (p !== eleName ? eleName : p));
     e.target.classList.add('active');
   };
 
   return (
     <Container>
       <Tabs className='tabs active' name='transcript' onClick={handleClick}>
-        Transcript <i></i>
+        Transcript
       </Tabs>
       <Tabs className='tabs' name='translation' onClick={handleClick}>
-        Translation <i></i>
+        Translation
       </Tabs>
       <Tabs className='tabs' name='TTS' onClick={handleClick}>
-        TTS <i></i>
+        TTS
       </Tabs>
       <Tabs className='tabs' name='V2V' onClick={handleClick}>
-        V2V <i></i>
+        V2V
+      </Tabs>
+      <Tabs className='tabs' name='custom' onClick={handleClick}>
+        Custom
       </Tabs>
     </Container>
   );
@@ -43,73 +41,32 @@ const Container = styled.nav`
   align-items: center;
   gap: 20px;
   background-color: var(--main-color);
+  position: relative;
   @media (max-width: 800px) {
     width: 69%;
     grid-template-rows: 1fr;
   }
   @media (min-width: 801px) {
-    width: 77%;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    width: 90%;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   }
 `;
 
 const Tabs = styled.div`
-  width: 70%;
+  width: 85%;
   justify-self: center;
   font-size: 1.5rem;
   font-weight: 700;
   padding: 1rem;
   border-radius: 10px;
-  transition: 0.2s;
+  transition: 0.5s;
   cursor: pointer;
   color: var(--doc-bg-color);
   position: relative;
-
+  box-shadow: transparent 0px 0px 0px 0px inset;
   &.active {
     background-color: var(--container-bg-color);
+    box-shadow: var(--home-tab-shadow);
     color: var(--main-color);
-  }
-
-  @media (min-width: 801px) {
-    &:hover {
-      translate: 0 10px;
-    }
-
-    &.active {
-      border-bottom-left-radius: 0;
-      border-bottom-right-radius: 0;
-      translate: 0 10px;
-    }
-    & > i::after {
-      content: '';
-      display: block;
-      height: 1rem;
-      width: 1rem;
-      background-color: transparent;
-      position: absolute;
-      bottom: 0;
-      right: -15px;
-      border-bottom-left-radius: 12px;
-      box-shadow: 0 6px 0 0.5px transparent;
-    }
-    & > i::before {
-      content: '';
-      display: block;
-      height: 1rem;
-      width: 1rem;
-      background-color: transparent;
-      position: absolute;
-      bottom: 0;
-      left: -15px;
-      border-bottom-right-radius: 12px;
-      box-shadow: 0 6px 0 0.5px transparent;
-    }
-    &.active > i::after {
-      box-shadow: 0 6px 0 0.5px var(--container-bg-color);
-    }
-
-    &.active > i::before {
-      box-shadow: 0 6px 0 0.5px var(--container-bg-color);
-    }
   }
 `;
